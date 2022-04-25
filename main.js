57211bf59b814a08673b2ab181a40ecbf10fc0c6
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2022-04-25 17:23:13 -0400

    teclado numerido funcional

--- main.js
+++ main.js
@@ -8,7 +8,47 @@ function Reproduzir(tecla){
 
 listaDeTeclas.forEach(tecla => {
     tecla.setAttribute('onclick', "Reproduzir(this), console.log(this.getAttribute('data-id'))");
+    let teclas = [97,98,99,100,101,102,103,104,105]        
 });
+
+document.addEventListener('keydown', (event) => {
+        var name = event.key;
+        var code = event.code;
+
+        switch (name) {
+            case "1":
+                document.querySelector(`#som_tecla_pom`).play()
+                break;
+            case "2":
+                document.querySelector(`#som_tecla_clap`).play()
+                break;
+            case "3":
+                document.querySelector(`#som_tecla_tim`).play()
+                break;
+            case "4":
+                document.querySelector(`#som_tecla_puff`).play()
+                break;
+            case "5":
+                document.querySelector(`#som_tecla_splash`).play()
+                break;
+            case "6":
+                document.querySelector(`#som_tecla_toim`).play()
+                break;
+            case "7":
+                document.querySelector(`#som_tecla_psh`).play()
+                break;
+            case "8":
+                document.querySelector(`#som_tecla_tic`).play()
+                break;
+            case "9":
+                document.querySelector(`#som_tecla_tom`).play()
+                break;
+        
+            default:
+                break;
+        }
+
+    }, false);
 
 function musica1(){
     let notas = [1,3,5,6,7,2,4,5]
@@ -19,11 +59,11 @@ function musica1(){
 }
 
 function musica2(){
-    let notas = [1,1,5,5,5,2,4,5]
-    notas.forEach(som => {
-        let nota = listaDeTeclas[som].getAttribute("data-id");
-        document.querySelector(`#som_tecla_${nota.toLowerCase()}`).play();        
-    });
+    let notas = [2,2,2,2,2,2,2,2,5,2,5]
+        notas.forEach(som => {
+            let nota = listaDeTeclas[som].getAttribute("data-id");
+            document.querySelector(`#som_tecla_${nota.toLowerCase()}`).play();        
+        });        
 }
 
 function musica0(){
@@ -32,10 +72,9 @@ function musica0(){
     let x = Math.floor(Math.random() * (max - min + 1)) + min;
     let nota = listaDeTeclas[x].getAttribute("data-id");
     document.querySelector(`#som_tecla_${nota.toLowerCase()}`).play();    
-}  
+} 
 
-const Sons = {
-    Musica(x){
+function Musica(x){
     switch(x){
         case 1:            
             musica1()
@@ -46,13 +85,16 @@ const Sons = {
         default:
             musica()
             break;
-    }}
+}}
+
+function Teclado(){
+    listaDeTeclas
 }
 
 function tocarMusica(x){
     const intervalo = setInterval(() => {
-        Sons.Musica(x)
-    }, 250);
+        Musica(x)
+    }, 300);
     setTimeout(() => {
         clearInterval(intervalo)
     }, 10000);
