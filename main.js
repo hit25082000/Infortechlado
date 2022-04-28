614aef99abb28fc290387e9f880f1fb9a52cce65
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2022-04-28 17:27:27 -0400

    Tentando salvar musicas

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
 const listaDeTeclas = document.querySelectorAll('.tecla')
 const audios = document.querySelectorAll('audio')
+var input = document.getElementById("CriarMusica");
 
-var tempoEntreTeclas;
 var Cronometro;
 let LogVelocidade = []
-var input = document.getElementById("CriarMusica");
-var clear = document.getElementById("BotaoApagar");
-
-function Reproduzir(tecla){    
-    let som = tecla.getAttribute("data-id")
-    if(document.querySelector(`#som_tecla_${som.toLowerCase()}`).currentTime > 0){
-        document.querySelector(`#som_tecla_${som.toLowerCase()}`).currentTime = 0
-    }     
-    document.querySelector(`#som_tecla_${som.toLowerCase()}`).play();
-    tecla.classList.add('ativa','active','focus')
-        setTimeout(() => {
-            tecla.classList.remove('ativa','active','focus')
-        }, 250);        
-}
 
 listaDeTeclas.forEach(tecla => {
     tecla.setAttribute('onclick', "Reproduzir(this), console.log(this.getAttribute('data-id'))");
@@ -45,83 +31,85 @@ input.addEventListener('keydown',(event) =>{
         event.preventDefault();
         document.getElementById("BotaoCriarMusica").click();
     }
-    event.key === "Backspace" ?   LogVelocidade.pop() : "";    
+    event.key === "Backspace" ?   LogVelocidade.pop() : "";  
+    
+    input == "" ? LogVelocidade = [] : "";
 });
 
-(()=>{
+const Musicas = {
 
-})()
-
-function musica1(){
-    let notas = [1,3,5,6,7,2,4,5]
-    notas.forEach((som,i)=> {   
-        setTimeout(() => {
-            Reproduzir(listaDeTeclas[som-1])             
-        }, i * 1000);
-    });
-}
-
-function musica2(){
-    let notas = [2,2,2,2,2,2,2,2,5,2,5]
-    notas.forEach((som,i)=> {   
-        setTimeout(() => {
-            Reproduzir(listaDeTeclas[som-1])             
-        }, i * 1000);
-    });       
-}
-
-
-
-function musica3(){
-
-    let notas = Array.from(String(input.value), Number);
-
-    console.log('notas',notas)
-    console.log("Log",LogVelocidade)
-    
-
-    for (let i = 0; i < notas.length; i++) {
-        let time = LogVelocidade[i]                
+    Musica1(){
+        let notas = [1,3,5,6,7,2,4,5]
+        notas.forEach((som,i)=> {   
             setTimeout(() => {
-                Reproduzir(listaDeTeclas[notas[i]-1])
-                
-                console.log("Index: ",i ,"Log: ",time )
-                console.log("Tecla: ",listaDeTeclas[notas[i]-1])
+                Reproduzir(listaDeTeclas[som-1])             
+            }, i * 1000);
+        });
+    },
+    
+    Musica2(){
+        let notas = [2,2,2,2,2,2,2,2,5,2,5]
+        notas.forEach((som,i)=> {   
+            setTimeout(() => {
+                Reproduzir(listaDeTeclas[som-1])             
+            }, i * 1000);
+        });       
+    },
+    
+    MusicaPadrao(){
+        max = Math.floor(8);
+        min = Math.ceil(0);
+        let x = Math.floor(Math.random() * (max - min + 1)) + min;
+        let notas = x.push
+        notas.forEach((som,i)=> {   
+            setTimeout(() => {
+                Reproduzir(listaDeTeclas[som-1])             
+            }, i * 1000);
+        });        
+    },
 
-            },time);
+    MusicaCriada(){
+
+        let notas = Array.from(String(input.value), Number);         
+    
+        for (let i = 0; i < notas.length; i++) {
+            let time = LogVelocidade[i]                
+                setTimeout(() => {
+                    Reproduzir(listaDeTeclas[notas[i]-1])                    
+                },time);
+        }
+    }, 
+}
+
+function Reproduzir(tecla){    
+    let som = tecla.getAttribute("data-id")
+    if(document.querySelector(`#som_tecla_${som.toLowerCase()}`).currentTime > 0){
+        document.querySelector(`#som_tecla_${som.toLowerCase()}`).currentTime = 0
+    }     
+    document.querySelector(`#som_tecla_${som.toLowerCase()}`).play();
+    tecla.classList.add('ativa','active','focus')
+        setTimeout(() => {
+            tecla.classList.remove('ativa','active','focus')
+        }, 200);        
+}
+
+function Salvar(){
+    Musicas += {
+        MusicaNova(){
+            let notas = input
+            let Log = LogVelocidade
+        notas.forEach((som,i)=> {   
+            setTimeout(() => {
+                Reproduzir(listaDeTeclas[som-1])             
+            }, Log[i]);
+        });
+        }
     }
+
+    let TabelaMusicas = document.querySelector(".Musicas")
+    let ListaMusicas = document.createElement("h1")
+
+    ListaMusicas.innerHTML = `<h1> Musica Nova </h1>`
+
+    TabelaMusicas.appendChild(ListaMusicas)
 }
-
-function musica(){
-    max = Math.floor(8);
-    min = Math.ceil(0);
-    let x = Math.floor(Math.random() * (max - min + 1)) + min;
-    let nota = listaDeTeclas[x].getAttribute("data-id");
-    document.querySelector(`#som_tecla_${nota.toLowerCase()}`).play();    
-} 
-
-function Musica(x){
-    switch(x){
-        case 1:            
-            musica1()
-            break;            
-        case 2:              
-            musica2()
-            break;
-        case 3:              
-            musica3()
-            break;
-        default:
-            musica()
-            break;
-}}
-
-function TocarMusica(x){
-    const intervalo = setInterval(() => {
-        Musica(x)
-    }, 500);
-    setTimeout(() => {
-        clearInterval(intervalo)
-    }, 10000);
-}
-
