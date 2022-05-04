2084efb72248e226cc9a0ccc342d8133dea0eb95
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2022-05-04 15:36:07 -0400

    Update

--- main.js
+++ main.js
@@ -9,30 +9,30 @@ listaDeTeclas.forEach(tecla => {
     tecla.setAttribute('onclick', "Reproduzir(this)");
 });
 
-input.addEventListener('keydown',(event)  => {
+input.addEventListener('keydown', (event) => {
     if (event.key === "Enter") {
         event.preventDefault();
-        document.getElementById("BotaoCriarMusica").click();        
+        document.getElementById("BotaoCriarMusica").click();
     }
 
-    if(event.key === "Backspace")
-         LogVelocidade.pop()   
+    if (event.key === "Backspace")
+        LogVelocidade.pop()
 
-    if(input == "")
-        LogVelocidade = [] 
+    if (input == "")
+        LogVelocidade = []
 
-    for (let i = 0; i < audios.length ; i++) {
-        if(event.key == (i+1)){
-            if(!LogVelocidade.length){
+    for (let i = 0; i < audios.length; i++) {
+        if (event.key == (i + 1)) {
+            if (!LogVelocidade.length) {
                 TempoAtual = (new Date()).getTime();
                 Cronometro = LogVelocidade.push((new Date(0)).getTime());
             }
-            else{
-                LogVelocidade.push(new Date().getTime() - TempoAtual);              
-            }  
-            Reproduzir(listaDeTeclas[i])                      
+            else {
+                LogVelocidade.push(new Date().getTime() - TempoAtual);
+            }
+            Reproduzir(listaDeTeclas[i])
         }
-    }        
+    }
 }, false);
 
 const Storage = {
@@ -47,113 +47,114 @@ const Storage = {
 
 const MusicConfig = {
 
-    Musica1(){
-        let notas = [1,3,5,6,7,2,4,5]
-        notas.forEach((som,i)=> {   
+    Musica1() {
+        let notas = [1, 3, 5, 6, 7, 2, 4, 5]
+        notas.forEach((som, i) => {
             setTimeout(() => {
-                Reproduzir(listaDeTeclas[som-1])             
+                Reproduzir(listaDeTeclas[som - 1])
             }, i * 1000);
         });
-    },   
-    
-    MusicaPadrao(){
+    },
+
+    MusicaPadrao() {
         max = Math.floor(8);
         min = Math.ceil(0);
         let x = Math.floor(Math.random() * (max - min + 1)) + min;
         let notas = x.push
-        notas.forEach((som,i)=> {   
+        notas.forEach((som, i) => {
             setTimeout(() => {
-                Reproduzir(listaDeTeclas[som-1])             
+                Reproduzir(listaDeTeclas[som - 1])
             }, i * 1000);
-        });        
+        });
     },
 
-    TocaMusica(notas,log){
+    TocaMusica(notas, log) {
         let Notas;
         let Log;
 
         notas == undefined ? Notas = input.value : Notas = notas;
-        log == undefined ? Log = LogVelocidade : Log = log;    
-    
-        for (let i = 0; i < Notas.length; i++) {
-            let time = Log[i]                
-                setTimeout(() => {
-                    Reproduzir(listaDeTeclas[Notas[i]-1])                    
-                },time);
-        }              
-    },     
+        log == undefined ? Log = LogVelocidade : Log = log;
 
-    Play(index){
-        MusicConfig.ListaMusicas.forEach((e,i) => {
-            if(index == i){
-                MusicConfig.TocaMusica(e.Notas,e.Log)
+        for (let i = 0; i < Notas.length; i++) {
+            let time = Log[i]
+            setTimeout(() => {
+                Reproduzir(listaDeTeclas[Notas[i] - 1])
+            }, time);
+        }
+        App.reload()
+    },
+
+    Play(index) {
+        MusicConfig.ListaMusicas.forEach((e, i) => {
+            if (index == i) {
+                MusicConfig.TocaMusica(e.Notas, e.Log)
             }
         });
     },
 
-    Remove(index){
-        MusicConfig.ListaMusicas.splice(index,1)  
-        App.reload()            
+    Remove(index) {
+        MusicConfig.ListaMusicas.splice(index, 1)
+        App.reload()
     },
 
-    ListaMusicas : Storage.get()
+    ListaMusicas: Storage.get()
 }
 
-class Musica{
-    constructor(Notas,Log,Name){
+class Musica {
+    constructor(Notas, Log, Name) {
         this.Name = Name
         this.Notas = Notas
-        this.Log = Log    
-        MusicConfig.ListaMusicas.push(this)   
+        this.Log = Log
+        MusicConfig.ListaMusicas.push(this)
     }
 }
 
-function Reproduzir(tecla){    
+function Reproduzir(tecla) {
     let som = tecla.getAttribute("data-id")
-    if(document.querySelector(`#som_tecla_${som.toLowerCase()}`).currentTime > 0){
+    if (document.querySelector(`#som_tecla_${som.toLowerCase()}`).currentTime > 0) {
         document.querySelector(`#som_tecla_${som.toLowerCase()}`).currentTime = 0
-    }     
+    }
     document.querySelector(`#som_tecla_${som.toLowerCase()}`).play();
-    tecla.classList.add('ativa','active','focus')
+    tecla.classList.add('ativa', 'active', 'focus')
     setTimeout(() => {
-        tecla.classList.remove('ativa','active','focus')
-    }, 200);        
+        tecla.classList.remove('ativa', 'active', 'focus')
+    }, 200);
 }
 
-function Salvar(){
+function Salvar() {
     let Notas = document.getElementById("CriarMusica").value.split('').map(Number);
     let Log = LogVelocidade
     let Name = document.querySelector('#NomeMusica').value
-    
-    new Musica(Notas,Log,Name)  
+
+    new Musica(Notas, Log, Name)
     Storage.set(MusicConfig.ListaMusicas)
 
     App.reload()
 }
 
-function Exibir(musica,index){
+function Exibir(musica, index) {
     let Name = musica.Name
 
-    let TabelaMusicas = document.querySelector(".MusicConfig")
+    let TabelaMusicas = document.querySelector(".Musicas tbody")
     let tr = document.createElement('tr')
     tr.innerHTML = `
-    <td onclick="MusicConfig.Play(${index})" style="cursor: pointer;" >${Name} </td>
+    <td class="XD" onclick="MusicConfig.Play(${index})" style="cursor: pointer;" ><h1>${Name} </h1></td>
     <td onclick="MusicConfig.Remove(${index})" style="cursor: pointer;"> X </td>
     `
     TabelaMusicas.appendChild(tr)
 }
 
 const App = {
-    init(){
+    init() {
         Storage.set(MusicConfig.ListaMusicas)
         MusicConfig.ListaMusicas.forEach(Exibir)
     },
 
-    reload(){
+    reload() {
         input.value = ""
         document.querySelector('#NomeMusica').value = ""
-        LogVelocidade = []  
-        let TabelaMusicas = document.querySelector(".MusicConfig")
+        LogVelocidade = []
+        let TabelaMusicas = document.querySelector(".Musicas tbody")
         TabelaMusicas.innerHTML = ""
         App.init()
     }
@@ -161,6 +162,6 @@ const App = {
 
 App.init()
 
-audios.forEach(x=>{
+audios.forEach(x => {
     x.volume = .3
 })
