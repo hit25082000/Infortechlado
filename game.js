088b02024117886158da7f487d6d030f62fed0e0
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2022-05-05 17:29:51 -0400

    Gamefic

--- game.js
+++ game.js
@@ -5,7 +5,8 @@ var nome = document.getElementById('NomeMusica')
 let TabelaMusicas = document.querySelector(".Musicas tbody")
 let volume = document.querySelector("#volume")
 var Cronometro;
-let LogVelocidade = []
+let LogAcertos = []
+let GameLog = []
 
 volume.oninput = () => {
     audios.forEach(x => {
@@ -17,25 +18,16 @@ listaDeTeclas.forEach(tecla => {
     tecla.setAttribute('onclick', "Reproduzir(this)");
 });
 
-notas.addEventListener('keydown', (event) => {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("BotaoCriarMusica").click();
-    }
-
-    if (event.key === "Backspace")
-        LimparCampos()
-
+document.addEventListener('keydown', (event) => {
     for (let i = 0; i < listaDeTeclas.length; i++) {
         if (event.key == (i + 1)) {
-            if (!LogVelocidade.length) {
+            if (!LogAcertos.length) {
                 TempoAtual = (new Date()).getTime();
-                Cronometro = LogVelocidade.push((new Date(0)).getTime());
+                Cronometro = LogAcertos.push((new Date(0)).getTime());
             }
             else {
-                LogVelocidade.push(new Date().getTime() - TempoAtual);
+                LogAcertos.push(new Date().getTime() - TempoAtual);
             }
-            Reproduzir(listaDeTeclas[i])
         }
     }
 }, false);
@@ -52,47 +44,52 @@ const Storage = {
 
 const MusicConfig = {
 
-    Musica1() {
-        let notas = [1, 3, 5, 6, 7, 2, 4, 5]
-        notas.forEach((som, i) => {
-            setTimeout(() => {
-                Reproduzir(listaDeTeclas[som - 1])
-            }, i * 1000);
-        });
-    },
-
-    MusicaPadrao() {
-        let notas = []
-        for (i = 0; i <= 99; i++) {
-            max = Math.floor(8);
-            min = Math.ceil(0);
-            let x = Math.floor(Math.random() * (max - min + 1)) + min;
-            notas.push(x)
-        }
-        notas.forEach((som, i) => {
-
-            setTimeout(() => {
-                Reproduzir(listaDeTeclas[som - 1])
-            }, i * 250);
-        });
-    },
-
     TocaMusica(NotasData, LogData) {
-
-        NotasData == undefined ? NotasData = notas.value : "";
-        LogData == undefined ? LogData = LogVelocidade : "";
 
         for (let i = 0; i < NotasData.length; i++) {
 
-            let time = LogData[i]
+            LogData[i] += 2000
+
+            GameLog.push(LogData[i] - 2000)
+
+            console.log("Game", GameLog[i])
+            console.log("Data", LogData[i])
+
+            setTimeout(() => {
+                listaDeTeclas[NotasData[i] - 1].classList.add('gamefic')
+                setTimeout(() => {
+                    listaDeTeclas[NotasData[i] - 1].classList.remove('gamefic')
+                }, 2000);
+            }, GameLog[i])
+
             setTimeout(() => {
                 Reproduzir(listaDeTeclas[NotasData[i] - 1])
-            }, time);
+            }, LogData[i]);
         }
         App.reload()
     },
 
     Play(index) {
+        document.querySelector('.modal-overlay').classList.toggle('active')
+        document.querySelector(".modal").innerHTML = 3
+        setTimeout(() => {
+            document.querySelector(".modal").innerHTML = 2
+            setTimeout(() => {
+                document.querySelector(".modal").innerHTML = 1
+                setTimeout(() => {
+                    document.querySelector(".modal").innerHTML = "START"
+                    setTimeout(() => {
+                        document.querySelector('.modal-overlay').classList.toggle('active')
+                        setTimeout(() => {
+                            MusicConfig.Game(index)
+                        }, 2000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    },
+
+    Game(index) {
         MusicConfig.ListaMusicas.forEach((musica, i) => {
             if (index == i) {
                 MusicConfig.TocaMusica(musica.Notas, musica.Log)
@@ -108,39 +105,12 @@ const MusicConfig = {
     ListaMusicas: Storage.get()
 }
 
-class Musica {
-    constructor(Notas, Log, Name) {
-        this.Name = Name
-        this.Notas = Notas
-        this.Log = Log
-        MusicConfig.ListaMusicas.push(this)
-    }
-}
-
 function Reproduzir(tecla) {
     let som = tecla.getAttribute("data-id")
     if (document.querySelector(`#som_tecla_${som.toLowerCase()}`).currentTime > 0) {
         document.querySelector(`#som_tecla_${som.toLowerCase()}`).currentTime = 0
     }
     document.querySelector(`#som_tecla_${som.toLowerCase()}`).play();
-    tecla.classList.add('ativa', 'active', 'focus')
-    setTimeout(() => {
-        tecla.classList.remove('ativa', 'active', 'focus')
-    }, 200);
-}
-
-function LimparCampos() {
-    nome.value = ""
-    notas.value = ""
-    LogVelocidade = []
-    nome.focus()
-}
-
-function Salvar() {
-    new Musica(notas.value.split('').map(Number), LogVelocidade, nome.value)
-    Storage.set(MusicConfig.ListaMusicas)
-
-    App.reload()
 }
 
 function Exibir(musica, index) {
@@ -162,8 +132,6 @@ const App = {
     },
 
     reload() {
-        LimparCampos()
-
         TabelaMusicas.innerHTML = ""
         App.init()
     }
