64552f45df7e9deb1110bd8093c116b2522cde95
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2022-05-12 17:28:50 -0400

    Sistema de Rank

--- game.js
+++ game.js
@@ -3,9 +3,12 @@ const audios = document.querySelectorAll('audio')
 let TabelaMusicas = document.querySelector(".Musicas tbody")
 let logAcertos = []
 let GameLog = []
-let musicaTocando;
+var musicaTocando = {
+    Log: [],
+    Notas: []
+};
+let pontos;
 let acerto;
-let pontuacao = [];
 
 listaDeTeclas.forEach(tecla => {
     tecla.setAttribute('onclick', "reproduzir(this)");
@@ -15,32 +18,40 @@ listaDeTeclas.forEach(tecla => {
 
 document.addEventListener('keydown', (event) => {
     for (let i = 0; i < listaDeTeclas.length; i++) {
-
         if (event.key.toLowerCase() == listaDeTeclas[i].id || event.code.toLowerCase() == listaDeTeclas[i].id) {
-
             anime(listaDeTeclas[i], 'act')
-
+            reproduzir(listaDeTeclas[i])
 
             acerto = (new Date().getTime() - TempoAtual);
-            logAcertos.push(acerto)
-
 
             let proxLog = musicaTocando.Log.find(x => x >= acerto)
             let proxNota = musicaTocando.Notas.find(x => x === musicaTocando.Notas[musicaTocando.Log.indexOf(proxLog)])
 
-            if (i + 1 == proxNota && acerto <= proxLog && acerto >= proxLog - 250) {
-                console.log("ACERTOU: ", acerto, "Log: ", proxLog, "Nota", proxNota)
+            if (acerto >= proxLog - 300) {//Acerto PERFEITO
+                if (i + 1 == proxNota) {
 
-                let div = document.querySelector(`.teclaImg${musicaTocando.Log.indexOf(proxLog)}`)
-                div.style.background = "red"
-                div.style.transition = "none"
-                pontuacao++
+                    musicaTocando.Notas.splice(musicaTocando.Log.indexOf(proxLog), 1)
+                    musicaTocando.Log.splice(musicaTocando.Log.indexOf(proxLog), 1)
 
-                console.log(pontuacao)
-            } else {
-                pontuacao--
+                    anime(listaDeTeclas[i], 'perfeito')
+
+                    logAcertos.push(3)
+                } else {
+                    logAcertos.push(0)
+                }
+            } else if (acerto >= proxLog - 1000) {//Acerto BOM
+                if (i + 1 == proxNota) {
+
+                    musicaTocando.Notas.splice(musicaTocando.Log.indexOf(proxLog), 1)
+                    musicaTocando.Log.splice(musicaTocando.Log.indexOf(proxLog), 1)
+
+                    anime(listaDeTeclas[i], 'bom')
+
+                    logAcertos.push(1)
+                } else {
+                    logAcertos.push(0)
+                }
             }
-
 
         }
     }
@@ -58,12 +69,16 @@ const Storage = {
 
 const MusicConfig = {
 
-    tocaMusica(NotasData, LogData) {
+    tocaMusica(NotasData, LogData, Name) {
         for (let i = 0; i < NotasData.length; i++) {
+
+            musicaTocando.Log.push(LogData[i])
+            musicaTocando.Notas.push(NotasData[i])
+            musicaTocando.Name = Name
+
             GameLog.push(LogData[i])
 
             GameLog[0] = 100
-
 
             let div = document.createElement('div')
 
@@ -74,20 +89,43 @@ const MusicConfig = {
             setTimeout(() => {
                 document.querySelector(`.teclaImg${i}`).classList.toggle('gamefic')
 
+                if (i == 0) {
+                    TempoAtual = new Date().getTime() + 5000;
+                }
+                console.log(TempoAtual)
                 setTimeout(() => {
                     div.remove()
                 }, 5000);
             }, GameLog[i])
 
             setTimeout(() => {
-                reproduzir(listaDeTeclas[NotasData[i] - 1])
                 if (i == NotasData.length - 1) {
-                    document.querySelector(".Musicas").classList.toggle('gameMode')
+                    setTimeout(() => {
+                        console.log(logAcertos)
+                        document.querySelector(".Musicas").classList.toggle('gameMode')
 
-                    App.reload()
-                }
-                if (i == 0) {
-                    TempoAtual = new Date().getTime();
+                        if (logAcertos.length == 0) {
+                            pontos = 0
+                        }
+                        if (logAcertos.length == 1) {
+                            pontos = logAcertos[0]
+                        }
+                        if (logAcertos.length > 1) {
+                            for (let index = 0; index <= logAcertos.length; index++)
+                                pontos += logAcertos[i]
+                        }
+
+                        document.querySelector('.modal-overlay').classList.toggle('active')
+
+                        document.querySelector(".modalRank").innerHTML = `
+                        <h2>CLASSIFICAÇÃO</h2>
+                        <p> Voçe fez: <strong>${pontos} pontos </strong></p>
+                        <p>Na musica : <strong>${musicaTocando.Name}</strong></p>
+                        <label for="NomeUsuario" class="labelNomeUsuario">Insira seu nome e salve seu resultado</label>
+                        <input type="text" id="NomeUsuario" placeholder=""><br>
+                        <input type="button" id="SalvarRank" onclick="SalvarRank(this)" value="Salvar">`
+
+                    }, 2000);
                 }
             }, LogData[i] + 5000);
 
@@ -99,7 +137,6 @@ const MusicConfig = {
         document.querySelector(".Musicas").classList.toggle('gameMode')
         document.querySelector('.modal-overlay').classList.toggle('active')
 
-
         document.querySelector(".modal").innerHTML = 3
         setTimeout(() => {
             document.querySelector(".modal").innerHTML = 2
@@ -109,8 +146,10 @@ const MusicConfig = {
                     document.querySelector(".modal").innerHTML = "START"
                     setTimeout(() => {
                         document.querySelector('.modal-overlay').classList.toggle('active')
+                        document.querySelector(".modal").innerHTML = ""
                         setTimeout(() => {
                             MusicConfig.procuraMusica(index)
+                            logAcertos = []
                         }, 0);
                     }, 1000);
                 }, 1000);
@@ -121,8 +160,7 @@ const MusicConfig = {
     procuraMusica(index) {
         MusicConfig.ListaMusicas.forEach((musica, i) => {
             if (index == i) {
-                musicaTocando = musica
-                MusicConfig.tocaMusica(musica.Notas, musica.Log)
+                MusicConfig.tocaMusica(musica.Notas, musica.Log, musica.Name)
             }
         });
     },
@@ -132,7 +170,9 @@ const MusicConfig = {
         App.reload()
     },
 
-    ListaMusicas: Storage.get()
+    ListaMusicas: Storage.get(),
+
+    ListaRanks: Storage.get()
 }
 
 function anime(element, classe) {
@@ -160,6 +200,25 @@ function exibir(musica, index) {
      </td>
     `
     TabelaMusicas.appendChild(tr)
+}
+
+class Rank {
+    constructor(logAcertos, Nome, Musica) {
+        this.Nome = Nome
+        this.Musica = Musica
+        this.logAcertos = logAcertos
+        MusicConfig.ListaRanks.push(this)
+    }
+}
+
+function SalvarRank() {
+    document.querySelector('.modal-overlay').classList.toggle('active')
+    let Nome = document.querySelector("#NomeUsuario")
+
+    new Rank(logAcertos, Nome, musicaTocando.Name)
+    Storage.set(MusicConfig.ListaRanks)
+
+    App.reload()
 }
 
 const App = {
